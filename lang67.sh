e2865410949d8e4a5e7181559349a97ccb0ee37c
memesmith67 <reddit.moderator.wizard@gmail.com>
2026-02-20 09:21:40 -0800

    Update and rename lang67.awk to lang67.sh
    asodifjaosdijfosdijf

---
+++ lang67.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+#licensed 2026 john morris beck gpl2 @ gnu.org
+#note to self use tmpfiles as arrays
+mcr(){ sh|sh;};
+lang67(){ awk '{j["declare"]="int i0,i1,i2,i3,i4,i5,i6,i7,i8,i9,ia,ib,ic,id,ie,if;void *p0,*p1,*p2,*p3,*p4,*p5,*p6,*p7",*p8,*p9,*pa,*pb,*pc,*pd,*pe,*pf;
+	j["loop"]="while("$2"){";
+	j["end"]="}";
+	j["return"]="return "$1;
+	j["size"]=$2"=sizeof("$3")";
+	j["operate"]=$2"="$3" "$4" "$5;
+	j["assign"]=$2"="$3;
+	j["get"]=$2 "=*("$3"*)("$4")";
+	j["set"]="*("$2"*)("$3")="$4;
+	j["jump"]=$2"=(void*)((char*)"$2" + (intptr_t)("$3"))";
+	j["allocate"]=$3"=malloc("$2")";
+	j["free"]="free("$2")";
+	j["read"]="fread("$2",1,(size_t)"$3",stdin)";
+	j["write"]="fwrite("$2",1,(size_t)"$3",stdout);fflush(stdout)";
+	if($0=="inline"){inline=!inline}
+	else if(inline){print}
+	else{print j[$1] ";"}}';};
+fastcc(){ gcc -O3 -march=native -ffast-math -flto -shared -fPIC -x c -o "$1.exe" -;};
