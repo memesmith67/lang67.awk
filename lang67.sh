a7531a5aa2dc3475d955abaa4c9a0cde16947b54
memesmith67 <reddit.moderator.wizard@gmail.com>
2026-02-20 22:11:32 -0800

    Update lang67.sh
    asodfaosidfjoaisjfdoisjdf

--- lang67.sh
+++ lang67.sh
@@ -5,7 +5,7 @@ lang67(){ awk '{j["declare"]="int ia,ib,ic,id,ie,if,ig,ih,ii,ij,ik,il,im,in,io,i
 	j["loop"]="while("$2"){";
 	j["end"]="}";
 	j["return"]="return "$2;
-	j["size"]=$2"=sizeof("$3")";
+   	j["size"]=$2"=sizeof("$3")";
 	j["operate"]=$2"="$3" "$4" "$5;
 	j["assign"]=$2"="$3;
 	j["get"]=$2 "=*("$3"*)("$4")";
@@ -21,13 +21,10 @@ lang67(){ awk '{j["declare"]="int ia,ib,ic,id,ie,if,ig,ih,ii,ij,ik,il,im,in,io,i
 	else if(inline){print}
 	else{print j[$1] ";"}}';};
 fastcc(){ gcc -O3 -march=native -ffast-math -flto -shared -fPIC -x c -o "$1.exe" -;};
-mcr_eval(){ cat "$1" > "/tmp/mcr$$"in;};
-eval_foolang(){ mcr_eval "/tmp/mcr$$foo";}
-mcrfoolang="$(cat << 'EOF'
+mcr(){  mkfifo "/tmp/mcr$$"in "/tmp/mcr$$"out; sh<"/tmp/mcr$$"in| sh>"/tmp/mcr$$"out &};
+mcr_send(){ cat > "/tmp/mcr$$"in;}
+foolang(){ mcr;
+mcr_send << 'EOF'
 some langauage
 EOF
-)"
-printf %s "$mcrfoolang" > "/tmp/mcr$$foo";
-mcr(){  mkfifo "/tmp/mcr$$"in "/tmp/mcr$$"out; sh<"/tmp/mcr$$"in| sh>"/tmp/mcr$$"out &};
-foolang(){ mcr;eval_foolang;};
-
+;};
